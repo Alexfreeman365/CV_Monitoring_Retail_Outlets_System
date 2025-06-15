a33b3d750991995b482d88afebd6fd280a7406c2
Aleksey_Sergeychuk <aleksey-sergeychuk@mail.ru>
2025-06-15 17:43:19 +0400

    feat(02_hiFTPCleaner): upgrade TextUI, add check_period, ignore .cache

--- 02_hiFTPCleaner.py
+++ 02_hiFTPCleaner.py
@@ -1,10 +1,8 @@
-import os
 import time
 from datetime import datetime, timedelta
-import ftplib
-import sys
 
 from funcs_FTP_access_cams_media_structure import *
+from funcs_TxtUI_request_app_description import *
 
 
 def create_txt_window_request(text_note_path):
@@ -76,36 +74,58 @@ def delete_out_days(ftp_host, ftp_user, ftp_pas, cams_out_days_dict):
                 pass
 
 
-def create_txt_msg(text):
-    with open(os.path.join(os.getcwd(), 'message.txt'), 'w') as f:
-        f.write(text)
+DESCRIPTION = (
+        'Поместите программу в папку с FTP загрузчиком hiFTPDloader, а точнее с его \n'
+        'сохраненными настройками _hiFTPconfig.dat. После успешного запуска программы \n'
+        'создайте для нее ярлык и перенесите его в папку автозагрузки Windows.\n'
+        '\n'
+        'hiFTPCleaner - программа для очистки FTP пространства, которое используют камеры.\n'
+        'Она через заданный промежуток проверяет наличие "старых" дней,\n' 
+        'которые не вошли в необходимый диапазон-окно, и удаляет их при наличии\n' 
+        'для каждой камеры на FTP-сервере для предотвращения его переполнения.\n' 
+        'Окно с необходимым количеством дней и промежуток времени для проверки задается\n'
+        'для этой программы в специальном файле-запросе hiFTPCleaner_request_app_description.\n' 
+        'Этот запрос появляется при первом запуске программы.\n' 
+        'Чтобы задать нужные параметры откройте запрос, заполните, сохраните и\n' 
+        'закройте этот файл.\n'
+    )
 
 
 if __name__ == '__main__':
-    cwd_path = os.getcwd() #r'H:\odfr_data\hiFTPCleaner_dlink'
-    text_note_path = os.path.join(cwd_path, 'hiFTPCleaner_Days_window_size_request.txt')
-    create_txt_window_request(text_note_path)
-    note_text = get_window(text_note_path)
+    # cwd_path = 'L:\Active_pjs\RG\cams_media'
+    # cwd_path = 'G:\hiFTPCleaner_beget'
+    cwd_path = os.getcwd()
+    request = f"Введите данные >>>\n" \
+              f"Временное окно в днях->:\nПериод проверки в часах->:\nЖурнал событий->: Нет\n{'-' * 30}"
+    data = request_app_description('02_hiFTPCleaner', cwd_path, request, DESCRIPTION)
+
+    note_text, check_period, ledger_msg = data
     str_window = note_text.split('-')[0]
+    ledger_flag = ledger_msg == 'Да'
 
     try:
         window = int(str_window)
-    except:
+        check_period = int(check_period)
+    except Exception as error:
+        if ledger_flag:
+            log_event(cwd_path, 'error', type(error).__name__)
+        print(error)
         sys.exit(0)
 
     try:
         usb_part = note_text.split('-')[1]
     except:
-        usb_part = 0
+        usb_part = ''
 
-    if len(str_window) != 0:
+    if window:
         while True:
             try:
-                ftp_host, ftp_user, ftp_pas = get_ftp_host_user_pas()
+                ftp_host, ftp_user, ftp_pas = get_ftp_host_user_pas(cwd_path)
                 cam_names = get_cam_names(ftp_host, ftp_user, ftp_pas)
+                cam_names = [name for name in cam_names if name != '.cache']
                 print(cam_names)
 
-                if usb_part != 0:
+                if usb_part:
                     cam_names = [usb_part+'/'+cam for cam in cam_names]
 
                 if len(cam_names) != 0:
@@ -115,10 +135,12 @@ if __name__ == '__main__':
                     cams_out_days_dict = get_cams_out_days_dict(cams_days_dict, dt_window_range)
                     if len(cams_out_days_dict) != 0:
                         delete_out_days(ftp_host, ftp_user, ftp_pas, cams_out_days_dict)
-            except:
-                pass
+            except Exception as error:
+                if ledger_flag:
+                    log_event(cwd_path, 'error', type(error).__name__)
+                print(error)
 
-            time.sleep(43200)
+            time.sleep(check_period * 3600)
             # break
     else:
         sys.exit(0)
