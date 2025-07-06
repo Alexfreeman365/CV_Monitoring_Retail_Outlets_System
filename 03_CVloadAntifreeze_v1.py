5e297fec7a597c74b2eb9b74724babc45be6c440
Aleksey_Sergeychuk <aleksey-sergeychuk@mail.ru>
2025-07-06 13:52:08 +0400

    feat(CVloadAntifreeze): add TxtUI, add load interval and reload period, add ledger

--- 03_CVloadAntifreeze_v1.py
+++ 03_CVloadAntifreeze_v1.py
@@ -1,6 +1,7 @@
 import os
 import subprocess, time, psutil
 
+from funcs_TxtUI_request_app_description import *
 
 def get_loaders(cwd_path):
     exes = [f for f in os.listdir(cwd_path) if f.split('.')[-1] == 'exe']
@@ -10,37 +11,54 @@ def get_loaders(cwd_path):
     return loaders
 
 
-def start_loaders(cwd_path, loaders):
+def start_loaders(cwd_path, loaders, load_interval):
     process = []
     for p in range(len(loaders)):
         proc = subprocess.Popen([os.path.join(cwd_path, loaders[p])], cwd=cwd_path)
         process.append(proc)
+        time.sleep(load_interval)
     return process
 
 
 def kill_loaders(process):
     for proc in process:
-        pobj = psutil.Process(proc.pid)
-        for c in pobj.children(recursive=True):
-            c.kill()
-        pobj.kill()
+        try:
+            pobj = psutil.Process(proc.pid)
+            for c in pobj.children(recursive=True):
+                c.kill()
+            pobj.kill()
+        except psutil.NoSuchProcess:
+            continue
+
+
+DESCRIPTION = (
+        'Программа CVloadAntifreeze сначала запускает все загрузчики из текущей папки,\n'        
+        'с необходимым интервалом, а затем через заданный период времени перезапускает их,\n'
+        'чтобы предотвратить их заморозку в случае отсутствия связи.\n'
+    )
 
 
 if __name__ == '__main__':
-
+    # cwd_path = r'G:\cams_media'
     cwd_path = os.getcwd()
-    loaders = get_loaders(cwd_path)
-    process = start_loaders(cwd_path, loaders)
+    app_name = '03_CVloadAntifreeze'
 
-    while True:
-        time.sleep(10800)
-        kill_loaders(process)
-        process = start_loaders(cwd_path, loaders)
+    request = f"Введите данные:\n" \
+              f"Интервал запуска загрузчиков (мин)->:\nПериод перезапуска загрузчиков (час)->:\n{'-' * 30}"
+    data = request_app_description(app_name, cwd_path, request, DESCRIPTION)
+    load_interval, reload_period = data
+    load_interval, reload_period = int(float(load_interval) * 60), int(float(reload_period) * 3600)
 
-    # try:
-    # except KeyboardInterrupt:
-    #     print('done')
+    try:
+        loaders = get_loaders(cwd_path)
+        process = start_loaders(cwd_path, loaders, load_interval)
 
+        while True:
+            time.sleep(reload_period)
+            kill_loaders(process)
+            process = start_loaders(cwd_path, loaders, load_interval)
+    except Exception as error:
+        log_event(cwd_path, app_name, 'error', type(error).__name__)
 
 
 
