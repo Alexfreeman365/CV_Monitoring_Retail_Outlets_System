c3ed537d8fda54f1c147f64a5a503bb1145fb439
Aleksey_Sergeychuk <aleksey-sergeychuk@mail.ru>
2025-07-06 13:59:57 +0400

    feat(CV_SYS): add the ability to close all own modules even after an error

--- CV_SYS_v1.py
+++ CV_SYS_v1.py
@@ -2,7 +2,6 @@ from keras.models import load_model
 import shutil
 import subprocess, psutil
 
-from funcs_initializer_camconfig_getcamframe import *
 from funcs_CV import *
 from funcs_vis_count_noseller_time import vis_count_noseller_pipeline
 from funcs_TxtUI_request_app_description import log_event
@@ -29,38 +28,40 @@ def backup_db():
     #     shutil.rmtree(os.path.join(cwd_path, 'db_backups', oldest_day))
 
 
-def start_hiFTPCleaner_CVloadAntifreeze(cwd_path):
+def start_hiFTPCleaner_CVloadAntifreeze(cwd_path, hiFTPCleaner=True):
     loc_path = os.path.join(cwd_path, 'cams_media')
-    # hiFTPCleaner_exe = [f for f in os.listdir(loc_path)
-    #                     if f.__contains__('hiFTPCleaner')
-    #                     and f.split('.')[-1] == 'exe'][0]
-    # hiFTPCleaner_proc = subprocess.Popen([os.path.join(loc_path, hiFTPCleaner_exe)], cwd=loc_path)
+    if hiFTPCleaner:
+        hiFTPCleaner_exe = [f for f in os.listdir(loc_path)
+                            if 'hiFTPCleaner' in f
+                            and f.split('.')[-1] == 'exe'][0]
+        hiFTPCleaner_proc = subprocess.Popen([os.path.join(loc_path, hiFTPCleaner_exe)], cwd=loc_path)
+    else:
+        hiFTPCleaner_proc = None
+
     CVloadAntifreeze_exe = [f for f in os.listdir(loc_path)
-                            if f.__contains__('CVloadAntifreeze')
+                            if 'CVloadAntifreeze' in f
                             and f.split('.')[-1] == 'exe'][0]
     CVloadAntifreeze_proc = subprocess.Popen([os.path.join(loc_path, CVloadAntifreeze_exe)], cwd=loc_path)
-    return [CVloadAntifreeze_proc] #[hiFTPCleaner_proc, CVloadAntifreeze_proc]
+    process = [hiFTPCleaner_proc, CVloadAntifreeze_proc]
+    return [p for p in process if p]
 
 
 def kill_hiFTPCleaner_CVloadAntifreeze(process):
     for proc in process:
-        pobj = psutil.Process(proc.pid)
-        for c in pobj.children(recursive=True):
-            try:
-                c.kill()
-            except:
-                pass
         try:
+            pobj = psutil.Process(proc.pid)
+            for c in pobj.children(recursive=True):
+                c.kill()
             pobj.kill()
-        except:
-            pass
+        except psutil.NoSuchProcess:
+            continue
 
 
 if __name__ == '__main__':
     cwd_path = os.getcwd()
     app_name = 'CV_SYS'
     ip_cam_data_paths_dict, cam_names = initializer()
-    process = start_hiFTPCleaner_CVloadAntifreeze(cwd_path) #1
+    process = start_hiFTPCleaner_CVloadAntifreeze(cwd_path, hiFTPCleaner=False) #1
 
     time.sleep(60 * 20) #2
 
@@ -71,19 +72,24 @@ if __name__ == '__main__':
         print('The system is loaded')
         log_event(cwd_path, app_name, 'sys', 'Starting the system')
 
-        while True:
-            for cam_name in sorted(cam_names):
-                cam_shapes_db_len = 0
-                if os.path.exists(os.path.join(cwd_path, 'db', f'{cam_name}_shapes_locs.csv')):
-                    df = pd.read_csv(os.path.join(cwd_path, 'db', f'{cam_name}_shapes_locs.csv'))
-                    cam_shapes_db_len = len(df)
-                    del df
+        try:
+            while True:
+                for cam_name in sorted(cam_names):
+                    cam_shapes_db_len = 0
+                    if os.path.exists(os.path.join(cwd_path, 'db', f'{cam_name}_shapes_locs.csv')):
+                        df = pd.read_csv(os.path.join(cwd_path, 'db', f'{cam_name}_shapes_locs.csv'))
+                        cam_shapes_db_len = len(df)
+                        del df
 
-                shape_detection(shape_detector, cam_shapes_db_len, ip_cam_data_paths_dict[cam_name],
-                                cam_name, cam_names)
+                    shape_detection(shape_detector, cam_shapes_db_len, ip_cam_data_paths_dict[cam_name],
+                                    cam_name, cam_names)
 
-                backup_db()
-                time.sleep(5)
+                    backup_db()
+                    time.sleep(5)
+        except Exception as error:
+            log_event(cwd_path, app_name, 'error', type(error).__name__)
+            kill_hiFTPCleaner_CVloadAntifreeze(process)
+            raise error
 
     except KeyboardInterrupt:
         for cam_name in sorted(cam_names):
