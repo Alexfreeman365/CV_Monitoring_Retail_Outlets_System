65e8e555d7a2925452434912cd7d207e80b944e5
Aleksey_Sergeychuk <aleksey-sergeychuk@mail.ru>
2025-11-17 21:16:11 +0400

    refactor(SysViscountEval): import common funcs: vis_count_noseller_time

--- 07_SysViscountEval_v1.py
+++ 07_SysViscountEval_v1.py
@@ -2,12 +2,9 @@ import os
 import sys
 import pandas as pd
 import numpy as np
-from datetime import datetime, timedelta
+from datetime import datetime
 
-from funcs_TxtUI_request_app_description import cleanup_mei_folders
-
-import atexit
-atexit.register(cleanup_mei_folders)
+from funcs_vis_count_noseller_time import short_name, visitors_counting
 
 
 def load_camconfig():
@@ -48,25 +45,6 @@ def read_txt_params(text_note_path):
     return params
 
 
-def base_columns_hours(cam_name):
-    camconfig = load_camconfig()
-    cam_set = [cam_set for cam_set in camconfig if cam_set['cam_name'] == cam_name][0]
-    hour_start = int(cam_set['work_hours'].split(',')[0][1:])
-    hour_end = int(cam_set['work_hours'].split(',')[1][:-1])
-    int_hours = np.arange(hour_start, hour_end)
-    cols = [str(hour) for hour in int_hours]
-    cols.insert(0, 'date')
-    cols.append('sum')
-    cols.append('s')
-    return cols, hour_start, hour_end
-
-
-def short_name(name):
-    if name[-1].isdigit():
-        name = name[:-1]
-    return name
-
-
 def dt_slice_shape_df(df_cam, dt_start, dt_end):
     df = df_cam.copy()
     dt_end_full = str(int(dt_end) + 1)
@@ -91,110 +69,7 @@ def find_new_shapes(cam_name, last_cam_visitors_day):
     return new_shapes, next_cam_visitors_day
 
 
-def visitors_counting(cam_name, new_shapes, date, mean_threshold, window_next, step_of_frames=1):
-    columns, hour_start, hour_end = base_columns_hours(cam_name)
-    if len(new_shapes) != 0:
-        new_shapes = new_shapes[new_shapes['cam_name'] == cam_name]
-        shapes = new_shapes[new_shapes['shape_zone'] == 1]
-        if len(shapes) != 0:
-            column = ['origin_file_name', 'cam_name']
-            shapes = shapes[column].copy()
-
-            shapes['date'] = shapes['origin_file_name'].apply(lambda x: int(x[0:6])).astype('str')
-            shapes['hour'] = shapes['origin_file_name'].apply(lambda x: int(x[6:8])).astype('str')
-
-            # Combining people into groups according to their frames and counting them
-            df_mto = shapes[shapes.duplicated(subset='origin_file_name', keep=False)]
-            df_mto_gr = (df_mto[df_mto.duplicated(subset='origin_file_name', keep=False)]
-                         .groupby('origin_file_name')['date'].count())
-            df_mto_gr = pd.DataFrame(df_mto_gr)
-            df_mto_gr.columns = ['people_num']
-
-            # Connecting group frames with single ones
-            df_ones = shapes.drop_duplicates(
-                subset='origin_file_name').set_index('origin_file_name')
-            df_pc = df_ones.join(df_mto_gr)
-            df_pc = df_pc.fillna(1)
-            df_pc.reset_index(inplace=True)
-            df_pc['people_num'] = df_pc['people_num'].astype('int')
-
-            # Creating a sample of frames according to the time step
-            if step_of_frames > 1:
-                df_pc = df_pc.copy().iloc[range(0, len(df_pc), step_of_frames)]
-
-            # Creating a quantitative shift to count the change
-            # in the number of people from frame to frame
-            df_pc['people_lag'] = df_pc['people_num'].shift(1)
-            df_pc = df_pc.fillna(1)
-            df_pc['people_lag'] = df_pc['people_lag'].astype('int')
-
-            df_pc = df_pc.fillna(1)
-            df_pc['people_diff'] = df_pc['people_num'] - df_pc['people_lag']
-            df_pc.loc[df_pc['people_diff'] < 0, 'people_diff'] = 0
-
-            def custom_rolling_mean(data, mean_threshold, window_next):
-                window = 1
-                result = []
-                for i in range(len(data)):
-                    if i < window:
-                        mean = 1
-                    else:
-                        start = max(i - window, 0)
-                        end = i
-                        mean = sum(data[start:end]) / (end - start)
-                        if mean <= mean_threshold:
-                            window = 1
-                        else:
-                            window = window_next
-                    result.append(mean)
-                result = result[1:]
-                result.append(1)
-                return result
-
-            df_pc['people_num_rol'] = custom_rolling_mean(
-                df_pc['people_num'], mean_threshold, window_next)
-            df_pc['people_lag_rol'] = custom_rolling_mean(
-                df_pc['people_lag'], mean_threshold, window_next)
-
-            df_pc = df_pc.fillna(1)
-            df_pc['people_diff_rol'] = df_pc['people_num_rol'] - df_pc['people_lag_rol']
-            df_pc.loc[df_pc['people_diff_rol'] < 0, 'people_diff_rol'] = 0
-
-            visitors = pd.pivot_table(
-                df_pc, values='people_diff_rol', index='date',
-                columns='hour', aggfunc='sum', fill_value=0).reset_index()
-            visitors.iloc[:, 1:12] = round(visitors.iloc[:, 1:12])
-            visitors['sum'] = visitors.iloc[:, 1:12].sum(axis=1)
-            visitors['s'] = 'auto'
-            visitors.columns.name = None
-            visitors['date'] = pd.to_datetime(visitors['date'], format='%y%m%d')
-
-            if len(visitors.columns) < len(columns):
-                visitors = pd.DataFrame(visitors, columns=columns).fillna(0)
-
-            int_columns = {c: 'int' for c in visitors.columns[1:-1]}
-            visitors = visitors.astype(int_columns)
-            visitors['date'] = visitors['date'].astype('str')
-
-        else:
-            hour_zero_values = np.zeros((1, hour_end - hour_start + 1), dtype=int)
-            visitors = pd.DataFrame(hour_zero_values, columns=columns[1:-1])
-            visitors['date'] = datetime.strptime(date, '%y%m%d')
-            visitors['date'] = visitors['date'].astype('str')
-            visitors['s'] = 'auto'
-            visitors = visitors[columns]
-    else:
-        hour_zero_values = np.zeros((1, hour_end - hour_start + 1), dtype=int)
-        visitors = pd.DataFrame(hour_zero_values, columns=columns[1:-1])
-        visitors['date'] = datetime.strptime(date, '%y%m%d')
-        visitors['date'] = visitors['date'].astype('str')
-        visitors['s'] = 'auto'
-        visitors = visitors[columns]
-
-    return visitors
-
-
-def evaluation(cam_name, params, camconfig):
+def evaluation(cam_name, params, camconfig, cwd_path):
     # Reading manual data
     xls = pd.ExcelFile(os.path.join(cwd_path, 'db', '1_real_viscount.xlsx'))
     all_real_visitors = pd.read_excel(xls, short_name(cam_name))
@@ -204,7 +79,6 @@ def evaluation(cam_name, params, camconfig):
     all_real_days_dt = np.unique(all_real_visitors['date'].dt.date)
     dt_to_str = lambda x: datetime.strftime(x, '%y%m%d')
     all_real_days = list(map(dt_to_str, all_real_days_dt))
-
 
     # Automatic calculation of the number of visitors
     cur_params = [cam_set['vis_count_alg'] for cam_set in camconfig if cam_set['cam_name'] == cam_name][0]
@@ -226,7 +100,7 @@ def evaluation(cam_name, params, camconfig):
             cam_shapes = pd.read_csv(os.path.join(cwd_path, 'db', f'{cam_name}_shapes_locs.csv'))
             new_shapes = dt_slice_shape_df(cam_shapes, day, day)
             new_shapes['cam_name'] = cam_name
-            day_visitors = visitors_counting(cam_name, new_shapes, day, mean_threshold, window_next)
+            day_visitors = visitors_counting(cam_name, new_shapes, day, mean_threshold, window_next, cwd_path=cwd_path)
             day_visitors['date'] = pd.to_datetime(day_visitors['date'])
             auto_visitors = pd.concat([auto_visitors, day_visitors])
 
@@ -279,7 +153,7 @@ def evaluation(cam_name, params, camconfig):
             cam_shapes = pd.read_csv(os.path.join(cwd_path, 'db', f'{cam_name}_shapes_locs.csv'))
             new_shapes = dt_slice_shape_df(cam_shapes, day, day)
             new_shapes['cam_name'] = cam_name
-            day_visitors = visitors_counting(cam_name, new_shapes, day, mean_threshold, window_next)
+            day_visitors = visitors_counting(cam_name, new_shapes, day, mean_threshold, window_next, cwd_path=cwd_path)
             day_visitors['date'] = pd.to_datetime(day_visitors['date'])
             auto_visitors = pd.concat([auto_visitors, day_visitors])
 
@@ -333,25 +207,19 @@ if __name__ == '__main__':
     if not os.path.exists(text_params_path):
         create_txt_params(text_params_path, camconfig)
     else:
-        try:
-            txt_params = read_txt_params(text_params_path)
+        txt_params = read_txt_params(text_params_path)
 
-            create_txt_program_status(text_program_status_path, msg='Ждите...')
+        create_txt_program_status(text_program_status_path, msg='Ждите...')
 
-            for cam_name in txt_params:
-                evaluation(cam_name, txt_params, camconfig)
+        for cam_name in txt_params:
+            print(cam_name)
+            evaluation(cam_name, txt_params, camconfig, cwd_path)
 
-            for cam_name in txt_params:
-                for cam_set in camconfig:
-                    if cam_set['cam_name'][:-1] == cam_name[:-1]:
-                        cam_set['vis_count_alg'] = txt_params[cam_name]
-            save_camconfig(camconfig)
+        for cam_name in txt_params:
+            for cam_set in camconfig:
+                if cam_set['cam_name'][:-1] == cam_name[:-1]:
+                    cam_set['vis_count_alg'] = txt_params[cam_name]
+        save_camconfig(camconfig)
 
-            create_txt_program_status(text_program_status_path, msg='Готово!')
-
-        except KeyboardInterrupt:
-            sys.exit(0)
-        except Exception as e:
-            print(f"Ошибка: {e}", file=sys.stderr)
-            sys.exit(1)
+        create_txt_program_status(text_program_status_path, msg='Готово!')
 
